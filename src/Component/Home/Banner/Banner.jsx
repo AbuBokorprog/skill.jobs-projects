e6649e2523d550fd830acafdc6de35c0ee2ae447
Abu Bokor <abubokor1066@gmail.com>
2023-04-10 11:42:10 +0600

    banner added

--- src/Component/Home/Banner/Banner.jsx
+++ src/Component/Home/Banner/Banner.jsx
@@ -2,18 +2,27 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="container-lg mx-20 flex items-center gap-10">
+    <div className="container-lg py-5 h-full mx-20 flex items-center">
       {/* banner details */}
-      <div>
-        <h2>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum,
-          tenetur.
-        </h2>
+      <div className="w-full">
+        <h1 className="text-6xl font-bold mx-20">
+          One Step Closer To Your{" "}
+          <span className="text-sky-500">Dream Job</span>
+        </h1>
+        <p className="mx-20">
+          Explore thousands of job opportunities with all the information you
+          need. Its your future. Come find it. Manage all your job application
+          from start to finish.
+        </p>
+        <button className="mx-20 btn bg-gradient-to-r from-cyan-500 to-blue-500">
+          Get Started
+        </button>
       </div>
       {/* banner img */}
       <div>
         <img
-          src="https://raw.githubusercontent.com/Porgramming-Hero-web-course/b7a9-career-hub-AbuBokorprog/main/src/assets/react.svg?token=GHSAT0AAAAAAB3YVZTBIQAMUICYYS2HTJ7KZBTU5AA"
+          className=""
+          src="https://img.freepik.com/free-photo/man-holding-something_1368-3677.jpg?w=826&t=st=1681103458~exp=1681104058~hmac=5ae66984000fa3bdcb95e2dbc1b2d7c6d4a450cc04a2e180dd6f6c33a1ad821d"
           alt=""
         />
       </div>
